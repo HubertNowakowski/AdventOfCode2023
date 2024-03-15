1e75c2893080bd3a96e9d89947b974ccd5a6bef2
Hubert Nowakowski <umberto.nowakowski@gmail.com>
2024-03-15 13:50:22 +0100

    Add day 8  part 2 bruteforce solution

--- DAY 8/wasteland.py
+++ DAY 8/wasteland.py
@@ -9,6 +9,15 @@ class Node():
     
     def __repr__(self) -> str:
         return f"{self.name} = ({self.left}, {self.right})\n"
+    
+    def is_endnode(self) -> bool:
+        return self.name[2] =='Z'
+    
+    def get_next(self, move: str) -> str:
+        if move == 'L': 
+            return self.left
+        elif move == 'R': 
+            return self.right
 
 
 if __name__ == "__main__":
@@ -17,26 +26,40 @@ if __name__ == "__main__":
     with open(input_file, "r") as f:
         lines = f.readlines()
     instructions = lines[0].strip()
-    # lines = lines[2:10]
 
     node_dict = { match[0]:Node(match[0], match[1], match[2]) \
-                 for l in lines for match in re.findall(r"([A-Z]+)\s=\s\(([A-Z]+),\s([A-Z]+)\)", l.strip()) }
+                 for l in lines for match in re.findall(r"([A-Z0-9]+)\s=\s\(([A-Z0-9]+),\s([A-Z0-9]+)\)", l.strip()) }
    
     # part one
-    current_node = 'AAA'
-    end_node = 'ZZZ'
+    # current_node = 'AAA'
+    # end_node = 'ZZZ'
+    # step_counter = 0
+
+    # print(f"start node: {current_node}")
+    # while current_node != end_node:
+    #     move = instructions[step_counter%len(instructions)]
+    #     # print( f"step: {step_counter} -> {move}" )
+    #     if move == 'L': current_node = node_dict[current_node].left
+    #     elif move == 'R': current_node = node_dict[current_node].right
+    #     else: 
+    #         raise ValueError
+    #     step_counter += 1
+
+    # print(f"end node: {current_node}\nsteps taken: {step_counter}")
+
+    # part two
+    # print(node_dict)
+    current_nodes = [node_dict[key] for key in node_dict.keys() if key[2]=='A']
+    print(current_nodes)
+
     step_counter = 0
 
-    print(f"start node: {current_node}")
-    while current_node != end_node:
+    while not all([n.is_endnode() for n in current_nodes]):
         move = instructions[step_counter%len(instructions)]
-        # print( f"step: {step_counter} -> {move}" )
-        if move == 'L': current_node = node_dict[current_node].left
-        elif move == 'R': current_node = node_dict[current_node].right
-        else: 
-            raise ValueError
+        if step_counter%1e9 == 0: 
+            print( f"step: {step_counter} -> {move} || {[n.is_endnode() for n in current_nodes]} || {[n.name for n in current_nodes]}" )
+        current_nodes = [node_dict[node.get_next(move)] for node in current_nodes]
         step_counter += 1
-
-    print(f"end node: {current_node}\nsteps taken: {step_counter}")
-
+    print(current_nodes)
+    print(f"steps taken: {step_counter}")
         
