c4fac72975d2a6761a987c655ba39674bc890052
Hubert Nowakowski <umberto.nowakowski@gmail.com>
2024-03-15 14:53:52 +0100

    Update day 8 part 2 solution This solution uses LCM method, which makes assumption, that length of path after reaching a XXZ node to the next end node is the same as the length from XXA to XXZ. The XXZ node is assumed to always be the same. I don't feel like that should be the case for any problem, but it seems to be true for the inputs generated in this task.

--- DAY 8/wasteland.py
+++ DAY 8/wasteland.py
@@ -1,4 +1,5 @@
 import re
+import functools
 
 
 class Node():
@@ -18,6 +19,29 @@ class Node():
             return self.left
         elif move == 'R': 
             return self.right
+
+def steps_to_next_end(node, instr_start):
+    global node_dict
+    current_node = node
+    step_counter = 0
+    
+    move = instructions[(instr_start+step_counter)%len(instructions)]
+    current_node = node_dict[current_node.get_next(move)]
+    step_counter += 1
+
+    while not current_node.is_endnode():
+        move = instructions[(instr_start+step_counter)%len(instructions)]
+        current_node = node_dict[current_node.get_next(move)]
+        step_counter += 1
+    return step_counter, current_node
+
+# helper math functions
+def gcd(a,b):
+    while b:
+        a,b = b, a%b
+    return a
+def lcm(a,b):
+    return a*b // gcd(a,b)
 
 
 if __name__ == "__main__":
@@ -52,14 +76,18 @@ if __name__ == "__main__":
     current_nodes = [node_dict[key] for key in node_dict.keys() if key[2]=='A']
     print(current_nodes)
 
-    step_counter = 0
+    path_lengths = [steps_to_next_end(node, 0) for node in current_nodes]
+    print(path_lengths)
+    path_lengths2 = [steps_to_next_end(node, start) for start, node in path_lengths]
+    print(path_lengths2)
 
-    while not all([n.is_endnode() for n in current_nodes]):
-        move = instructions[step_counter%len(instructions)]
-        if step_counter%1e9 == 0: 
-            print( f"step: {step_counter} -> {move} || {[n.is_endnode() for n in current_nodes]} || {[n.name for n in current_nodes]}" )
-        current_nodes = [node_dict[node.get_next(move)] for node in current_nodes]
-        step_counter += 1
-    print(current_nodes)
-    print(f"steps taken: {step_counter}")
-        
+    # if path length from A->Z and then Z->Z is the same then we can calculate LCM and be done
+    path_check = [pl1[0]==pl2[0] for pl1, pl2 in zip(path_lengths, path_lengths2)]
+    print(path_check)
+    # all true (i also see that its the same end node, end nodes loop)
+    if all(path_check):
+        lengths = [l for l, node in path_lengths]
+        LCM = functools.reduce(lambda x, y: lcm(x, y), lengths)
+        print(LCM)
+    else:
+        print("Path length A->Z and Z->Z is not always equal")
